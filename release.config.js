ed67a5d71c6ea985179bd6c1984979d70f437675
Kathy Lee <katlee@microsoft.com>
2022-05-03 13:27:29 -0700

    removing versioning

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  branches: "main",
-  repositoryUrl: "https://github.com/katlee-org/react-app",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
-  ],
-};
